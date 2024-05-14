e40303aeda4ce84f5c0d85d71dfce9445c5c6b35
Li He <80284104+Li-8023@users.noreply.github.com>
2024-05-14 11:41:49 +0800

    pop up window done

--- src/views/centerRight1.vue
+++ src/views/centerRight1.vue
@@ -3,7 +3,7 @@
     <div class="bg-color-black">
       <div class="d-flex pt-2 pl-2">
         <span>
-           <i class='bx bx-expand-alt bx-tada bx-rotate-90 expand-icon' style="font-size: 20px;"></i>
+           <i class='bx bx-expand-alt bx-tada bx-rotate-90 expand-icon' style="font-size: 20px;" @click="toggleModal"></i>
         </span>
         <div class="d-flex">
           <span class="text mx-2" style="font-size: 20px;">企业得分排行榜</span>
@@ -14,6 +14,32 @@
         <dv-scroll-board class="dv-scr-board" :config="scrollConfig" />
       </div>
     </div>
+
+    <!-- Pop-up window -->
+    <div v-if="showModal" class="modal">
+      <div class="modal-content">
+        <span @click="toggleModal" class="close-button">
+          <i class='bx bx-exit-fullscreen bx-tada'></i>
+        </span>
+        <table>
+          <thead>
+            <tr>
+               <th v-for="header in config.header" :key="header">{{ header }}</th>
+            </tr>
+          </thead>
+          <tbody>
+             <tr v-for="(row, index) in filteredData" :key="index">
+              <td v-for="(cell, cellIndex) in row" :key="cellIndex">
+                <!-- <a v-if="cellIndex === 1" :href="getUrl(cell)" target="_blank">{{ cell }}</a> -->
+                <a v-if="cellIndex === 1" :href="getUrl()" target="_blank">{{ cell }}</a>
+                <template v-else>{{ cell }}</template>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +48,7 @@ export default {
   props:['searchQuery'],
   data() {
     return {
+      showModal:false,
       config: {
         header: ['当月排名','名称', '得分', '评价类别'],
         rowNum: 16, //表格行数
@@ -54,7 +81,28 @@ export default {
           ['18','分宜县龙华金属回收有限公司第二门市部', '98', "D"],
           ['19','云桉建筑', '96', "D"],
           ['20','江西肆海电子科技有限公司', '93', "D"],
+          ['21','新余市金车精测电子科技有限公司', '106', "C"],
+          ['22','江西中邦电子科技有限公司', '105', "C"],
+          ['23','江西盛泰精密光学有限公司', '103', "C"],
+          ['24','中联神农高科技有限公司', '102', "A"],
+          ['25','中联神农高科技有限公司', '102', "A"],
+          ['26','逊强贸易有限公司', '101', "D"],
+          ['27','新余景润服饰有限公司', '99', "D"],
+          ['28','分宜县龙华金属回收有限公司第二门市部', '98', "D"],
+          ['29','云桉建筑', '96', "D"],
+          ['30','江西肆海电子科技有限公司', '93', "D"],
         ],
+    }
+  },
+  methods:{
+    //  getUrl(name) {
+    //   return `http://example.com/profile/${name}`;
+    // },
+    getUrl(){
+      return "http://localhost:8080/#/detail";
+    },
+    toggleModal(){
+      this.showModal = !this.showModal;
     }
   },
    computed:{
@@ -107,4 +155,68 @@ $box-width: 760px;
     cursor: pointer;
   }
 }
+
+.modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background: #0e101f;
+  padding: 20px;
+  border-radius: 5px;
+  overflow: auto; 
+  max-height: 90%;
+  max-width: 100%;
+}
+
+table {
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 50px 30px;
+
+  th, td {
+    border: 1px solid #0e101f;
+    padding: 30px;
+    text-align: left;
+    font-size: 30px;
+    background-color: #0e101f;
+  }
+
+  th {
+    padding: 30px;
+    font-size: 22px;
+  }
+
+  a {
+    color: inherit; 
+    text-decoration: underline; 
+  }
+
+  a:hover {
+    color: white; 
+    text-decoration: none; 
+  }
+}
+
+.close-button{
+  font-size: 40px;
+  color:white;
+  cursor:pointer;
+  border-radius: 10px;
+  position: fixed;
+  margin-left: 1200px;
+  margin-top: -10px;
+}
+.close-button:hover{
+  color: red;
+}
+
 </style>
